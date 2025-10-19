54f57105052489cb7b127788cafc3885d2e9c0d5
akirilov <akirilov91@gmail.com>
2025-10-19 14:54:43 +0000

    Initial attempt - percent based

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,83 @@
 mod save_monitor;
-use std::env::args;
+use std::{borrow::Cow, env::args};
+use std::fs::read_dir;
+use colored::Colorize;
 
-use crate::save_monitor::load_save_file;
+use crate::save_monitor::{parse_save_file, WorldStats};
+
+const save_dir_path: &str = "example";
+const warn_threshold: f64 = -5.0;
+const alarm_threshold: f64 = -10.0;
 
 fn main() {
-    let result = load_save_file(args().nth(1).unwrap().as_str());
-    println!("{result:?}");
+    let mut saves: Vec<WorldStats> =  Vec::new();
+    let save_dir =read_dir(save_dir_path);
+    if save_dir.is_err() {
+        println!("Error reading save directory");
+        return;
+    }
+    for entry in save_dir.unwrap() {
+        match entry {
+            Ok(entry) => {
+                let path = entry.path();
+                if path.is_file() && path.extension().is_some_and(|f| f == "save") {
+                    match parse_save_file(path.to_string_lossy().as_ref()) {
+                        Ok(result) => saves.push(result),
+                        Err(e) => println!("Error loading save file: {e}")
+                    }
+                }
+            }
+            Err(e) => println!("Error reading file: {e}")
+        }
+    }
+    saves.sort();
+    let mut last_save: Option<&WorldStats> = None;
+    for cur_save in saves.iter() {
+        if last_save.is_some() {
+            match cur_save.diff(last_save.unwrap()) {
+                Ok(diff) => {
+                    pretty_print_diff(diff);
+                }
+                Err(e) => println!("Diff Error: {e}")
+            }
+        }
+        println!("{}", cur_save.filename);
+        last_save = Some(&cur_save);
+    }
+
+}
+
+fn pretty_print_diff(diff: save_monitor::WorldStatsDiff) -> () {
+    let total_things = match diff.total_things {
+        n if n < alarm_threshold => format!("{:.2}", n).red(),
+        n if n < warn_threshold => format!("{:.2}", n).yellow(),
+        n => format!("{:.2}", n).white(),
+    };
+    let total_rooms = match diff.total_rooms {
+        n if n < alarm_threshold => format!("{:.2}", n).red(),
+        n if n < warn_threshold => format!("{:.2}", n).yellow(),
+        n => format!("{:.2}", n).white(),
+    };
+    let total_pipe_networks = match diff.total_pipe_networks {
+        n if n < alarm_threshold => format!("{:.2}", n).red(),
+        n if n < warn_threshold => format!("{:.2}", n).yellow(),
+        _ => format!("{:.2}", diff.total_pipe_networks).white(),
+    };
+    let total_cable_networks = match diff.total_cable_networks {
+        n if n < alarm_threshold => format!("{:.2}", n).red(),
+        n if n < warn_threshold => format!("{:.2}", n).yellow(),
+        _ => format!("{:.2}", diff.total_cable_networks).white(),
+    };
+    let total_atmospheres = match diff.total_atmospheres {
+        n if n < alarm_threshold => format!("{:.2}", n).red(),
+        n if n < warn_threshold => format!("{:.2}", n).yellow(),
+        _ => format!("{:.2}", diff.total_atmospheres).white(),
+    };
+    let total_damage = match diff.total_damage {
+        n if n > (-1.0 * alarm_threshold) => format!("{:.2}", n).red(),
+        n if n > (-1.0 * warn_threshold) => format!("{:.2}", n).yellow(),
+        _ => format!("{:.2}", diff.total_damage).white(),
+    };
+    println!("Diff: Things: {}% Rooms: {}% PipeNets: {}% CableNets: {}% Atmos: {}% Damage: {}% Players: [{}]", total_things, total_rooms, total_pipe_networks, total_cable_networks, total_atmospheres, total_damage, diff.players.join(", ").yellow());
+
 }
--- src/save_monitor.rs
+++ src/save_monitor.rs
@@ -1,48 +1,91 @@
 use quick_xml::{events::Event, Reader};
+use std::cmp::Ordering;
 use std::error;
 use std::fs::File;
 use tempfile::tempdir;
 use zip::ZipArchive;
+use conv::ValueFrom;
 
 const WORLD_META_XML: &str = "world_meta.xml";
 const WORLD_XML: &str = "world.xml";
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct WorldStats {
-    DateTime: usize,
-    TotalThings: usize,
-    TotalRooms: usize,
-    TotalPipeNetworks: usize,
-    TotalCableNetworks: usize,
-    TotalAtmospheres: usize,
-    TotalDamage: usize,
-    Players: Vec<String>,
+    pub date_time: usize,
+    pub total_things: usize,
+    pub total_rooms: usize,
+    pub total_pipe_networks: usize,
+    pub total_cable_networks: usize,
+    pub total_atmospheres: usize,
+    pub total_damage: f64,
+    pub players: Vec<String>,
+    pub filename: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct WorldStatsDiff {
+    pub date_time: usize,
+    pub total_things: f64,
+    pub total_rooms: f64,
+    pub total_pipe_networks: f64,
+    pub total_cable_networks: f64,
+    pub total_atmospheres: f64,
+    pub total_damage: f64,
+    pub players: Vec<String>,
 }
 
 impl WorldStats {
-    pub fn new() -> Self {
+    pub fn new(filename: String) -> Self {
         WorldStats {
-            DateTime: 0,
-            TotalThings: 0,
-            TotalRooms: 0,
-            TotalPipeNetworks: 0,
-            TotalCableNetworks: 0,
-            TotalAtmospheres: 0,
-            TotalDamage: 0,
-            Players: Vec::new(),
+            date_time: 0,
+            total_things: 0,
+            total_rooms: 0,
+            total_pipe_networks: 0,
+            total_cable_networks: 0,
+            total_atmospheres: 0,
+            total_damage: 0.0,
+            players: Vec::new(),
+            filename: filename,
         }
+    }
+
+    pub fn diff(&self, other: &WorldStats) -> Result<WorldStatsDiff> {
+        let mut players_diff = Vec::new();
+        for sp in self.players.iter() {
+            if !other.players.contains(sp) {
+                players_diff.push(sp.clone());
+            }
+        }
+        Ok(WorldStatsDiff {
+            date_time: self.date_time - other.date_time,
+            total_things: (f64::value_from(self.total_things)? - f64::value_from(other.total_things)?) / f64::value_from(self.total_things)?,
+            total_rooms: (f64::value_from(self.total_rooms)? - f64::value_from(other.total_rooms)?) / f64::value_from(self.total_rooms)?,
+            total_pipe_networks: (f64::value_from(self.total_pipe_networks)? - f64::value_from(other.total_pipe_networks)?) / f64::value_from(self.total_pipe_networks)?,
+            total_cable_networks: (f64::value_from(self.total_cable_networks)? - f64::value_from(other.total_cable_networks)?) / f64::value_from(self.total_cable_networks)?,
+            total_atmospheres: (f64::value_from(self.total_atmospheres)? - f64::value_from(other.total_atmospheres)?) / f64::value_from(self.total_atmospheres)?,
+            total_damage: (self.total_damage - other.total_damage) / self.total_damage,
+            players: players_diff
+        })
     }
 }
 
-type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+impl Eq for WorldStats {}
 
-pub fn load_save_file(path: &str) -> Result<WorldStats> {
+impl Ord for WorldStats {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.date_time.cmp(&other.date_time)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+pub fn parse_save_file(path: &str) -> Result<WorldStats> {
     // Create a temporary directory and extract the zip file
     let temp_dir = tempdir()?;
     ZipArchive::new(File::open(path)?)?.extract(temp_dir.path())?;
 
     // Prepare a WorldStats struct to hold the statistics
-    let mut world_stats = WorldStats::new();
+    let mut world_stats = WorldStats::new(path.to_string());
 
     // Read the world_meta data
     {
@@ -55,29 +98,29 @@ pub fn load_save_file(path: &str) -> Result<WorldStats> {
                     let text = String::from_utf8(e.local_name().into_inner().to_vec())?;
                     path_stack.push(text);
                 }
-                Ok(Event::End(e)) => {
+                Ok(Event::End(_)) => {
                     path_stack.pop();
                 }
                 Ok(Event::Text(e)) => {
                     let text: &str = &e.decode()?;
                     match path_stack.last() {
                         Some(s) if s == "DateTime" => {
-                            world_stats.DateTime = text.parse::<usize>()?
+                            world_stats.date_time = text.parse::<usize>()?
                         }
                         Some(s) if s == "NumberOfThings" => {
-                            world_stats.TotalThings = text.parse::<usize>()?
+                            world_stats.total_things = text.parse::<usize>()?
                         }
                         Some(s) if s == "NumberOfRooms" => {
-                            world_stats.TotalRooms = text.parse::<usize>()?
+                            world_stats.total_rooms = text.parse::<usize>()?
                         }
                         Some(s) if s == "NumberOfPipeNetworks" => {
-                            world_stats.TotalPipeNetworks = text.parse::<usize>()?
+                            world_stats.total_pipe_networks = text.parse::<usize>()?
                         }
                         Some(s) if s == "NumberOfCableNetworks" => {
-                            world_stats.TotalCableNetworks = text.parse::<usize>()?
+                            world_stats.total_cable_networks = text.parse::<usize>()?
                         }
                         Some(s) if s == "NumberOfAtmospheres" => {
-                            world_stats.TotalAtmospheres = text.parse::<usize>()?
+                            world_stats.total_atmospheres = text.parse::<usize>()?
                         }
                         None => (),
                         _ => (),
@@ -97,27 +140,37 @@ pub fn load_save_file(path: &str) -> Result<WorldStats> {
         let mut buf = Vec::new();
         let mut path_stack = Vec::new();
         let mut get_player_name = false;
+        let mut damage_state = false;
         loop {
             match reader.read_event_into(&mut buf) {
                 Ok(Event::Start(e)) => {
                     let text = String::from_utf8(Vec::from(e.local_name().into_inner()))?;
-                    path_stack.push(text);
+                    // Look for damage
+                    if &text == "DamageState" {
+                        damage_state = true;
+                    }
                     // Look for players
                     let attributes = String::from_utf8(e.attributes_raw().to_vec())?;
                     if attributes.contains("HumanSaveData") {
-                        // Set a flag to grab the next "CustomName" tag
-                        get_player_name = true;
+                        get_player_name = true; // Set a flag to grab the next "CustomName" tag
                     }
+                    path_stack.push(text);
                 }
                 Ok(Event::End(e)) => {
-                    path_stack.pop();
+                    let text = path_stack.pop();
+                    if text.is_some() && text.unwrap() == "DamageState" {
+                        damage_state = false;
+                    }
                 }
                 Ok(Event::Text(e)) => {
-                    let text: &str = &e.decode()?;
+                    let text = e.decode()?.trim().to_owned();
                     match path_stack.last() {
                         Some(s) if s == "CustomName" && get_player_name => {
-                            world_stats.Players.push(text.to_string());
-                            get_player_name = false;
+                            world_stats.players.push(text.into());
+                            get_player_name = false; // Clear the flag after grabbing the name
+                        }
+                        Some(s) if damage_state && !text.is_empty()=> {
+                            world_stats.total_damage += text.parse::<f64>()?;
                         }
                         None => (),
                         _ => (),
