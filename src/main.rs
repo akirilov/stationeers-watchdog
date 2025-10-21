5924a6278b3d09594c6cc5479e8c51b456071bb8
akirilov <akirilov91@gmail.com>
2025-10-21 05:04:46 +0000

    Update run args

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use colored::Colorize;
 
 use crate::save_monitor::{parse_save_file, WorldStats};
 
-const save_dir_path: &str = "example";
 const warn_threshold: isize = -50;
 const alarm_threshold: isize = -100;
 const damage_warn_threshold: f64 = 500.0;
 const damage_alarm_threshold: f64 = 1000.0;
-const type_threshold: isize = -10;
+const type_warn_threshold: isize = -10;
+const type_alarm_threshold: isize = -20;
 
 fn pretty_print_diff(diff: save_monitor::WorldStatsDiff) -> () {
     let total_atmospheres = match diff.total_atmospheres {
@@ -23,14 +23,26 @@ fn pretty_print_diff(diff: save_monitor::WorldStatsDiff) -> () {
         n if n > damage_warn_threshold => format!("{:.2}", n).yellow(),
         _ => format!("{:.2}", diff.total_damage).white(),
     };
+    let diff_sum: isize = diff.type_map.values().sum();
+    let type_map = match diff_sum {
+        n if n < type_alarm_threshold => format!("{:?}", diff.type_map).red(),
+        n if n < type_warn_threshold => format!("{:?}", diff.type_map).yellow(),
+        _ => format!("{:?}", diff.type_map).white(),
+    };
     println!("Diff: Atmos: {} Damage: {} Players: [{}]", total_atmospheres, total_damage, diff.players.join(", ").yellow());
-    println!("  Types: {:?}", diff.type_map);
+    println!("  Types: {}", type_map);
 
 }
 
 fn main() {
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <save_directory>", args[0]);
+        return;
+    }
+    let save_dir_path = &args[1];
     let mut saves: Vec<WorldStats> =  Vec::new();
-    let save_dir =read_dir(save_dir_path);
+    let save_dir = read_dir(save_dir_path);
     if save_dir.is_err() {
         println!("Error reading save directory");
         return;
